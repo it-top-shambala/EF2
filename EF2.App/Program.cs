0a4205337d38b84b98c7309072db926b8c924cbd
Starinin Andrey <starininandrey@gmail.com>
2022-01-26 21:17:35 +0300

    Update

---
+++ EF2.App/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using EF2.DB_Lib;
+
+namespace EF2.App
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var db = new BookStore();
+
+            foreach (var book in db.TabBooks.ToList())
+            {
+                Console.WriteLine($"#{book.Id}: {book.Title}, {book.IdAuthorNavigation.LastName} {book.IdAuthorNavigation.FirstName}, {book.IdGenreNavigation.Name}");
+            }
+        }
+    }
+}
